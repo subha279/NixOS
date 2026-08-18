6b28ff7125ff5e904f39b70d1a59a32471985aa7
subha279 <111702137+subha279@users.noreply.github.com>
2026-08-18 15:05:00 +0530

    refactor

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -8,12 +8,50 @@
 
     audio.enable = true;
 
-    pulse.enable = true;
-
     alsa.enable = true;
-
     alsa.support32Bit = true;
 
+    pulse.enable = true;
+
     jack.enable = true;
+
+    wireplumber = {
+      enable = true;
+
+      extraConfig = {
+        "10-bluetooth" = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+          };
+
+          "monitor.bluez.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "bluez_card.F4_4E_FC_42_90_A5";
+                }
+              ];
+
+              actions = {
+                update-props = {
+                  "device.profile" = "a2dp-sink";
+                  "bluez5.auto-connect" = [
+                    "a2dp_sink"
+                  ];
+                };
+              };
+            }
+          ];
+        };
+
+        "11-bluetooth-policy" = {
+          "wireplumber.settings" = {
+            "bluetooth.autoswitch-to-headset-profile" = false;
+          };
+        };
+      };
+    };
   };
 }
--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ ... }:
 
 {
   time.timeZone = "Asia/Kolkata";
@@ -7,14 +7,26 @@
 
   console.keyMap = "us";
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
-  nix.settings.substituters = lib.mkForce [
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-  ];
+    # 1. Boost concurrent downloads (default is only 25)
+    http-connections = 50;
+
+    # 2. Allow Nix to use all CPU cores if it has to build something locally
+    cores = 0;
+    max-jobs = "auto";
+
+    # 3. Restore the official cache and use Asian mirrors safely
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 
