933a9269fa54cfa210f065ed8ace95c012fed79a
subha279 <111702137+subha279@users.noreply.github.com>
2026-08-21 23:13:09 +0530

    xD

--- home/hyprland/config/monitor.lua
+++ home/hyprland/config/monitor.lua
@@ -1,71 +1,27 @@
 --------------------------------------------------
 -- Monitor Configuration
--- https://wiki.hypr.land/Configuring/Monitors/
 --------------------------------------------------
 
 --------------------------------------------------
--- Default Monitor
---------------------------------------------------
-
---hl.monitor({
---    output = "",
---    mode = "preferred",
---    position = "auto",
---    scale = "1.25",
---})
-
---------------------------------------------------
--- Example Laptop Display
---------------------------------------------------
--- Uncomment and modify if you want to configure
--- your laptop display manually.
-
--- hl.monitor({
---     output = "eDP-1",
---     mode = "1920x1080@60",
---     position = "0x0",
---     scale = 1.25,
--- })
-
---------------------------------------------------
--- Example External Monitor
---------------------------------------------------
-
-hl.monitor({
-	output = "HDMI-A-1",
-	mode = "1920x1080@180",
-	position = "0x0",
-	scale = 1,
-})
-
---------------------------------------------------
--- Example DisplayPort Monitor
---------------------------------------------------
-
--- hl.monitor({
---     output = "DP-1",
---     mode = "2560x1440@180",
---     position = "0x0",
---     scale = 1,
--- })
-
---------------------------------------------------
--- Example Vertical Monitor
---------------------------------------------------
-
--- hl.monitor({
---     output = "DP-2",
---     mode = "1080x1920@60",
---     position = "2560x0",
---     scale = 1,
---     transform = 1,
--- })
-
---------------------------------------------------
--- Example Disable Monitor
+-- Laptop Display
 --------------------------------------------------
 
 hl.monitor({
 	output = "eDP-1",
-	disabled = true,
+--	mode = "1920x1080@60.00800",
+--	position = "0x0",
+--	scale = 1.25,
+    disabled = true
+})
+
+--------------------------------------------------
+-- External Acer VG240Y M3
+--------------------------------------------------
+
+hl.monitor({
+	output = "HDMI-A-1",
+	mode = "1920x1080@180.00301",
+	--position = "1920x0",
+	position = "0x0",
+	scale = 1,
 })
