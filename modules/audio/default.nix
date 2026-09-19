20624a8f6d5e905551fb52f7ae7a8b1ac748599c
subha279 <111702137+subha279@users.noreply.github.com>
2026-09-19 19:55:48 +0530

    cleanup

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -1,57 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   security.rtkit.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    easyeffects
+  ];
+
   services.pipewire = {
     enable = true;
-
     audio.enable = true;
 
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
 
     pulse.enable = true;
-
     jack.enable = true;
 
-    wireplumber = {
-      enable = true;
-
-      extraConfig = {
-        "10-bluetooth" = {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-          };
-
-          "monitor.bluez.rules" = [
-            {
-              matches = [
-                {
-                  "device.name" = "bluez_card.F4_4E_FC_42_90_A5";
-                }
-              ];
-
-              actions = {
-                update-props = {
-                  "device.profile" = "a2dp-sink";
-                  "bluez5.auto-connect" = [
-                    "a2dp_sink"
-                  ];
-                };
-              };
-            }
-          ];
-        };
-
-        "11-bluetooth-policy" = {
-          "wireplumber.settings" = {
-            "bluetooth.autoswitch-to-headset-profile" = false;
-          };
-        };
-      };
-    };
+    wireplumber.enable = true;
   };
 }
